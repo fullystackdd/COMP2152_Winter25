8ff9ec3a765c4911bd5269b4ca2d0094676c1a86
Leonard <101511592@georgebrown.ca>
2025-01-16 17:40:17 -0500

    New Lab

---
+++ lab1.py
@@ -0,0 +1,20 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Leonard
+# Last Name: Eriyo
+
+
+a = 1; b = 2
+c = 3; d = 4
+
+
+e = (a - (b ** c // d)) + (a % c)
+
+temperature = 32.6
+
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Please enter your age: "))
+
+userAge += 22
+
+print("Now showing the shop items filtered by age: {}".format(userAge))
